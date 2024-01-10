b7cda9e956eab6999fafbb8e6c3435c55ee69461
LauraKuzara <kuzaralaura@gmail.com>
2024-01-10 12:29:37 -0500

    added froyou object

--- froyo.js
+++ froyo.js
@@ -1,4 +1,8 @@
-let userInput = prompt ("Please input your froyo flavors separated by a comma");
+let userInput = prompt ("Please input your froyo flavors separated by a comma and space:");
+
+let froyo = {
+    flavors: vanilla, chocolate, strawberry, salted carmel, blueberry
+}
 
 let splitArr = userInput.split(", ");
 
