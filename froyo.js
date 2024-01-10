a241f816d6ca396400e7714dff01daedae314eb8
LauraKuzara <kuzaralaura@gmail.com>
2024-01-10 15:55:35 -0500

    moved array of flavors into froyo object

--- froyo.js
+++ froyo.js
@@ -1,9 +1,7 @@
-let userInput = prompt ("Please input your froyo flavors separated by a comma and space:");
-
-let flavors = [vanilla, chocolate, strawberry, blueberry, birthday cake];
+let userInput = prompt ("Please input your froyo ${flavors} separated by a comma and space:");
 
 let froyo = {
-    flavors: vanilla, chocolate, strawberry, blueberry
+    flavors: ["vanilla", "chocolate", "strawberry", "blueberry"],
 }
 
 let splitArr = userInput.split(", ");
