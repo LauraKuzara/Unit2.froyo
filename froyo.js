894133db36075c40f1e53045bbe6680213d7a6ca
LauraKuzara <kuzaralaura@gmail.com>
2024-01-10 15:58:08 -0500

    moved variables around to account for scope

--- froyo.js
+++ froyo.js
@@ -1,9 +1,10 @@
-let userInput = prompt ("Please input your froyo ${flavors} separated by a comma and space:");
-
-let froyo = {
-    flavors: ["vanilla", "chocolate", "strawberry", "blueberry"],
-}
+let userInput = prompt ("Please input your froyo flavors separated by a comma and space:");
 
 let splitArr = userInput.split(", ");
 
 console.log(splitArr);
+
+let froyo = {
+    flavors: ["vanilla", "chocolate", "strawberry", "blueberry"],
+};
+
