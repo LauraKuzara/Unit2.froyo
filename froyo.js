25621bafe789cefe66a13f9f89a6bd90b166b045
LauraKuzara <kuzaralaura@gmail.com>
2024-01-10 15:50:54 -0500

    add an array for flavors

--- froyo.js
+++ froyo.js
@@ -1,7 +1,9 @@
 let userInput = prompt ("Please input your froyo flavors separated by a comma and space:");
 
+let flavors = [vanilla, chocolate, strawberry, blueberry, birthday cake];
+
 let froyo = {
-    flavors: vanilla, chocolate, strawberry, salted carmel, blueberry
+    flavors: vanilla, chocolate, strawberry, blueberry
 }
 
 let splitArr = userInput.split(", ");
